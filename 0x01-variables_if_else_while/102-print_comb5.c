7ae611003a0aa826f6985e29f044dce0c0f336df
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-03 22:28:21 -0700

    fishing into

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ for (i = 48; i <= 57; i++)
 for (k = 48; k <= 57; k++)
 {
 for (j = 48; j <= 57; j++)
-{														
+{												
 if (((k + j) > (c + i) &&  k >= c) || c < k)
 {
 putchar(c);
 putchar(i);
-putchar(' ');															
-putchar(k);	
+putchar(' ');
+putchar(k);
 putchar(j);
 
 if (c + i + k + j == 227 && c == 57)
