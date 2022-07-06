b2ec64f289aaed87792ed02d755b5afc8d1f92d0
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-06 13:29:48 -0700

    hands on

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,6 @@
 
 /**
  * print_times_table - prints the times table for n.
- *
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
@@ -18,31 +17,31 @@ for (j = 0; j <= n; j++)
 res = (i * j);
 if (j != 0)
 {
-_putchar(',');
-_putchar(' ');
+	_putchar(',');
+	_putchar(' ');
 }
 if (res < 10 && j != 0)
 {
-_putchar(' ');
-_putchar(' ');
-_putchar((res % 10) + '0');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar((res % 10) + '0');
 }
 else if (res >= 10 && res < 100)
 {
-_putchar(' ');
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
+	_putchar(' ');
+	_putchar((res / 10) + '0');
+	_putchar((res % 10) + '0');
 }
 else if (res >= 100 && j != 0
 {
-_putchar((res / 100) + '0');
-_putchar((res / 10) % 10 + '0');
-_putchar((res % 10) + '0');
+	_putchar((res / 100) + '0');
+	_putchar((res / 10) % 10 + '0');
+	_putchar((res % 10) + '0');
 }
 else
-_putchar((res % 10) + '0');
+	_putchar((res % 10) + '0');
 }
-_putchar('\n');
+	_putchar('\n');
 }
 }
 }
