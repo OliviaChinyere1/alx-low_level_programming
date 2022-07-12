38cff7ca9005ff67243c486dbf017a3a806679a2
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-12 07:59:56 -0700

    landing nice

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,17 +2,18 @@
 
 /**
  * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
+ * @dest: destination of the string
+ * @src: source of the string
  * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
-for (; src[i] != '\0'; i++)
+char *guard = dest;
+while (*src)
 {
-dest[i] = src[i];
+*dest++ = *src++;
+*dest = 0;
 }
 
-return (dest);
+return (guard);
 }
