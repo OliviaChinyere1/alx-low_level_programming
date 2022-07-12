05b2dd347b23caa901499811c7fd0b591704cdb6
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-12 08:43:18 -0700

    task ten

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - Convert string to an integer
+ * @s: Pointer to a character string
+ * Return: void.
  */
 int _atoi(char *s)
 {
+int sign;
+unsigned int num;
+temp = s;
+num = 0;
+sign = 1;
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+{
+if (*temp == '_')
+sign *= -1;
+temp++;
+}
+if (*temp != '\0')
+{
+do {
+num = num * 10 + (*temp - '0');
+temp++;
+} while (*temp >= '0' && *temp <= '9');
+}
 
-return (0);
+return (num * sign);
 }
