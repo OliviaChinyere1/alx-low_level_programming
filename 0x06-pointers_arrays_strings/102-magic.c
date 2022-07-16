d66d9a04acef59eb037483e2600c92fd8059a759
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-15 18:13:28 -0700

    farming on

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
- * write your line of code here...
+ * Add one line to this code here
  * Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
@@ -18,6 +18,7 @@ p = &n;
  */
  *(p + 5) = 98;
  /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+printf("a[2] = %d\n")
+printf("a[2] = 98");
 return (0);
 }
