76def25c97de73fdbc6b7a240641cf4b299fba87
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-07-26 18:43:42 -0700

    going into

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * _isspace - check if a character is whitespace
+ * @c: the character to check
+ *
+ * Return: 1 is c is a whitespace character, otherwise 0
+ */
+int _isspace(int c)
+{
+if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
+return (1);return (0);
+}
 
 /**
  * strtow - split a string into words
