68314d1ebf572add507c3e9d1a821f9ba5f61d45
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-04 06:59:10 -0700

    finding on

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,6 @@
  * print_name - f points to either func in main
  * @name: name
  * @f: pointer to print upp or non upp func
- *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
