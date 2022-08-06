33b7f3c2a8503aa4c0565fd23d380f5a547d6c40
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-05 17:25:12 -0700

    going on

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,7 +8,7 @@
  * @f: pointer to print upp or non upp func
  * Return: void
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *));
 {
 if (name == NULL || f == NULL)
 return;
