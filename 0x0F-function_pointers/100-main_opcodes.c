d9c75f0816858666f6ca8ac1a9b60a47f0b3041e
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-05 18:54:04 -0700

    gooding well

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 {
 		int count, bytes;
 
-		if (argc ! = 2)
+		if (argc != 2)
 		{
 			printf("Error\n");
 			exit(1);
 		}
 
-		bytes =atoi(argv[1]);
+		bytes = atoi(argv[1]);
 		if (bytes < 0)
 		{
 			printf("Error\n");
