b6225013bfeddb9a9fed4136f8b58f6bbf996366
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-05 19:44:35 -0700

    standing all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,11 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_stat(arg;ist, n);
+	va_start(arglist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arg;ist, int);
+		sum += va_arg(arglist, int);
 	}
 	va_end(arglist);
 
