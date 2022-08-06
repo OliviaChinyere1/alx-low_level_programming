225a67aa11eac65d2fe914a32f6f392107a44293
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-05 17:43:30 -0700

    fishing on

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,20 +9,8 @@
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
-	unsigned int i;
-
-	printf("game, my uppercase name is ");
-	i = 0:
-	while (name[i])
-	{
-		if (name[i] >= 'a' && name[i] <= 'z')
-		{
-			putchar(name[i] + 'A' - 'a');
-		}
-		else
-		{
-			putchar(name[i]);
-		}
-		i++;
-	}
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
