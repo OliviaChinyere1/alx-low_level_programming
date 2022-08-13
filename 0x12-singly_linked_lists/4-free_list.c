2318b7346235f5b6efca296f47b3335c5baaea86
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-12 21:40:00 -0700

    fishing

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,20 +6,20 @@
  **/
 void free_list(list_t *head)
 {
-list_t *actual_node;
-list_t *next_node;
-if (head)
-{
-actual_node = head;
-next_node = head->next;
-while (next_node)
-{
-free(actual_node->str);
-free(actual_node);
-actual_node = next_node;
-next_node = next_node->next;
-}
-free(actual_node->str);
-free(actual_node)
+	list_t *actual_node;
+	list_t *next_node;
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+		free(actual_node->str);
+		free(actual_node);
+		actual_node = next_node;
+		next_node = next_node->next;
+		}
+		free(actual_node->str);
+		free(actual_node);
 }
 }
