108dee94f000a2f0a3dae56b9d3659b7d5d502fe
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-08-17 08:35:15 -0700

    came on

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,6 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: constant pointer type list_t
- *
  * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const listint_t *h)
